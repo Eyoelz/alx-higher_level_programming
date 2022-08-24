b2fd28947a556e2c0bf7d5d87d65efb9939a54b2
root <root@DESKTOP-CJ7P98Q.localdomain>
2022-08-24 05:04:51 +0300

    new

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+number = 3.14454454
+print (f"{:.2f} Float: ".format(number))
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+name = "Holberton School"
+word_first_3 = name [0:3]
+word_last_2 = name [-2:]
+middle_word = name [1:8]
+print ("First 3 letters: ", word_first_3)
+print ("Last 2 letters: ", word_last_2)
+print ("Middle word: ", middle_word)
