a0173cade640455130b880ca4fa1690dd3852b1b
root <root@DESKTOP-CJ7P98Q.localdomain>
2022-08-24 06:14:50 +0300

    new

--- 0x00-python-hello_world/5-print_spring.py
+++ 0x00-python-hello_world/5-print_spring.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 name_1 = "Holberton School"
 name_2 = name_1[0:9]
-print (name_1 * 3)
-print (name_2 )
+print(name_1 * 3)
+print(name_2 )
--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-name = "Holberton School"
-word_first_3 = name [0:3]
-word_last_2 = name [-2:]
-middle_word = name [1:8]
-print ("First 3 letters: ", word_first_3)
-print ("Last 2 letters: ", word_last_2)
-print ("Middle word: ", middle_word)
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
